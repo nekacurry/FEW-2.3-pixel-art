0705d5b082dcb03d9ba569eca7cd53946e8fe997
nekacurry <anneka498@gmail.com>
2021-07-13 18:14:01 -0500

    attempt to fix router

--- src/index.js
+++ src/index.js
@@ -8,13 +8,19 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 
+import { HashRouter } from 'react-router-dom'
+
+
+
 const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
