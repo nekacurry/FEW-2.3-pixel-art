3ef080bc6ec075311dc6d3126fd9407c3989127d
nekacurry <anneka498@gmail.com>
2021-07-13 21:35:40 -0500

    changed name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1 className="App-title">Pixely Redux</h1>
+          <h1 className="App-title">PixelP Redux</h1>
       </header>
      <Grid />
      <ColorPicker />
